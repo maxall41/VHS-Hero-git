c0d7b8a99261ce2c1dc6c4f245dc5c7d942d357f
max <maxall4@protonmail.com>
2021-06-28 14:38:17 -0700

    Fixed issues with snapping

--- VHS Hero/Assets/scripts/Gameplay/LevelManager.cs
+++ VHS Hero/Assets/scripts/Gameplay/LevelManager.cs
@@ -22,14 +22,18 @@ public class LevelManager : MonoBehaviour
 
     private float cooldownDisplay = 3.5F;
 
-    private bool onStart;
-
     public TextMeshProUGUI travelStatusText;
 
     public GameObject flicker;
 
+    public GameObject transHolder;
+
     GameObject closetsObject;
     private float oldDistance = 9999;
+
+    GameObject[] NearGameobjects;
+
+    private float nextLevelCooldown; // Fixes issues with timing
 
     public Slider slider;
 
@@ -39,7 +43,6 @@ public class LevelManager : MonoBehaviour
     {
         lastLevel = Instantiate(levels[currentLevel], new Vector3(0, 2.9F, 0), Quaternion.identity);
         currentLevel++;
-        onStart = true;
         playerStartPos = GameObject.Find("Player").transform.position;
 
     }
@@ -57,20 +60,32 @@ public class LevelManager : MonoBehaviour
 
     private void Snap(GameObject gb)
     {
+        Debug.Log(gb.transform.position);
         // Snap to nearest point to prevent player falling off map.
+        closetsObject = FindClosestSnap();
+        gb.transform.position = new Vector3(closetsObject.transform.position.x, closetsObject.transform.position.y, 0);
+        //gb.transform.position = new Vector3(gb.transform.position.x, gb.transform.position.y, 0);
+        Debug.Log("Snapped to " + closetsObject.name);
+    }
 
-        GameObject[] NearGameobjects = GameObject.FindGameObjectsWithTag("Snap");
-        foreach (GameObject g in NearGameobjects)
+    public GameObject FindClosestSnap()
+    {
+        GameObject[] gos;
+        gos = GameObject.FindGameObjectsWithTag("Snap");
+        GameObject closest = null;
+        float distance = Mathf.Infinity;
+        Vector3 position = transform.position;
+        foreach (GameObject go in gos)
         {
-            float dist = Vector3.Distance(GameObject.Find("Player").transform.position, g.transform.position);
-            if (dist < oldDistance)
+            Vector3 diff = go.transform.position - GameObject.Find("Player").transform.position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance)
             {
-                closetsObject = g;
-                oldDistance = dist;
+                closest = go;
+                distance = curDistance;
             }
         }
-        gb.transform.position = new Vector3(closetsObject.transform.position.x, closetsObject.transform.position.y + 2, 0);
-        //gb.transform.position = new Vector3(gb.transform.position.x, gb.transform.position.y, 0);
+        return closest;
     }
 
     private void Update()
@@ -78,13 +93,16 @@ public class LevelManager : MonoBehaviour
         timeCooldown -= Time.deltaTime;
        pullbackTimer -= Time.deltaTime;
         cooldownDisplay += Time.deltaTime;
+        nextLevelCooldown -= Time.deltaTime;
         slider.value = cooldownDisplay;
 
         if (pullbackTimer < 0 && pullbacked == true)
         {
             // Pulls player back to current level
+            GameObject.Find("Player").transform.parent = transHolder.transform;
             Destroy(lastLevel);
             lastLevel = Instantiate(levels[lastLevelIndex], new Vector3(0,2.9F, 0), Quaternion.identity);
+            GameObject.Find("Player").transform.parent = lastLevel.transform;
             pullbacked = false;
             GameObject.Find("SFX Manager").GetComponent<sfxManager>().F_pullback();
 
@@ -107,21 +125,27 @@ public class LevelManager : MonoBehaviour
 
     public void NextLevel()
     {
-
-        if (currentLevel > levels.Length - 1)
+        if (nextLevelCooldown < 0)
         {
-            SceneManager.LoadScene("credits");
+            if (currentLevel > levels.Length - 1)
+            {
+                SceneManager.LoadScene("credits");
 
-        } else
-        {
-            Debug.Log(currentLevel);
-            StartCoroutine("Flicker"); // Make screen flicker
-            GameObject.Find("SFX Manager").GetComponent<sfxManager>().F_timeTravel(); // Play time travel sound effect
-            Destroy(lastLevel);
-            lastLevel = Instantiate(levels[currentLevel], new Vector3(0, 2.9F, 0), Quaternion.identity);
-            currentLevel++;
-            onStart = false;
+            }
+            else
+            {
+                GameObject.Find("Player").transform.parent = transHolder.transform;
+                Debug.Log(currentLevel);
+                StartCoroutine("Flicker"); // Make screen flicker
+                GameObject.Find("SFX Manager").GetComponent<sfxManager>().F_timeTravel(); // Play time travel sound effect
+                Destroy(lastLevel);
+                lastLevel = Instantiate(levels[currentLevel], new Vector3(0, 2.9F, 0), Quaternion.identity);
+                GameObject.Find("Player").transform.parent = lastLevel.transform;
+                currentLevel++;
+            }
+            nextLevelCooldown = 0.5F;
         }
+
     }
 
     IEnumerator Flicker()
@@ -149,24 +173,18 @@ public class LevelManager : MonoBehaviour
             if (timeCooldown < 0)
             {
                 StartCoroutine("Flicker"); // Make screen flicker
-                Destroy(lastLevel); // Remove old level
+            GameObject.Find("Player").transform.parent = transHolder.transform;
+            Destroy(lastLevel); // Remove old level
 
                 GameObject.Find("SFX Manager").GetComponent<sfxManager>().F_timeTravel(); // Play time travel sound effect
 
 
-                // Weird mess of code
-                if (onStart == false)
-                {
-                    lastLevelIndex = currentLevel - 1;
-
-                }
-                else
-                {
-                    lastLevelIndex = 0;
-                }
+                 // Weird mess of code
+                lastLevelIndex = currentLevel - 1;
                 lastLevel = Instantiate(levels[currentLevel - 2], new Vector3(0, 2.9F, 0), Quaternion.identity);
-                // Snap to nearest point
-                Snap(GameObject.Find("Player"));
+            GameObject.Find("Player").transform.parent = lastLevel.transform;
+            // Snap to nearest point
+            Snap(GameObject.Find("Player"));
                 // Stuff
                 pullbackTimer = pullbackTime;
                 pullbacked = true;
@@ -181,22 +199,16 @@ public class LevelManager : MonoBehaviour
         if (timeCooldown < 0)
         {
             StartCoroutine("Flicker"); // Make screen flicker
+            GameObject.Find("Player").transform.parent = transHolder.transform;
             Destroy(lastLevel); // Remove old level
 
             GameObject.Find("SFX Manager").GetComponent<sfxManager>().F_timeTravel(); // Play time travel sound effect
 
 
             // Weird mess of code
-            if (onStart == false)
-            {
-                lastLevelIndex = currentLevel - 1;
-
-            } else
-            {
-                lastLevelIndex = 0;
-            }
+            lastLevelIndex = currentLevel - 1;
             lastLevel = Instantiate(levels[currentLevel], new Vector3(0, 2.9F, 0), Quaternion.identity);
-
+            GameObject.Find("Player").transform.parent = lastLevel.transform;
             // Snap to nearest point
             Snap(GameObject.Find("Player"));
             // Stuff
