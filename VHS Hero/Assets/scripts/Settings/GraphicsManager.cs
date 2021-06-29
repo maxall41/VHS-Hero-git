dc2a5b5a4bab49f46f10fffe2ef82916e2cb9bb4
max <maxall4@protonmail.com>
2021-06-28 20:54:28 -0700

    Added light flickering

--- VHS Hero/Assets/scripts/Settings/GraphicsManager.cs
+++ VHS Hero/Assets/scripts/Settings/GraphicsManager.cs
@@ -7,5 +7,6 @@ public class GraphicsManager : MonoBehaviour
     public void UpdateGraphics(int qualityIndex)
     {
         QualitySettings.SetQualityLevel(qualityIndex);
+        PlayerPrefs.SetInt("qualityIndex",qualityIndex);
     }
 }
