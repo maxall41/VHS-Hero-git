56f86180df61159d360e1a32bfa725dcf85adb1a
max <maxall4@protonmail.com>
2021-07-09 20:43:14 -0700

    Adjusted climb

--- VHS Hero/Assets/scripts/Gameplay/Player/ClimbCheck.cs
+++ VHS Hero/Assets/scripts/Gameplay/Player/ClimbCheck.cs
@@ -4,20 +4,10 @@ using UnityEngine;
 
 public class ClimbCheck : MonoBehaviour
 {
-   
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (this.gameObject.GetComponentInParent<PlayerDataHolder>().ClimbWall == true && this.gameObject.GetComponentInParent<PlayerMovement>().Grounded == false) {
-            if (collision.gameObject.tag == "Ground")
-            {
-                this.gameObject.GetComponentInParent<PlayerMovement>().DoubleJumpCount = 0;
-                this.gameObject.GetComponentInParent<Rigidbody2D>().velocity = new Vector2(0, 0);
-                this.gameObject.GetComponentInParent<Rigidbody2D>().gravityScale = 0;
-                this.gameObject.GetComponentInParent<PlayerMovement>().Climbing = true;
-                this.gameObject.GetComponentInParent<PlayerMovement>().Grounded = false;
-            }
-        }
-    }
+
+    // Start is called before the first frame update
+    Vector2 vector2 = new Vector2(0, 0);
+
 
     private void OnTriggerStay2D(Collider2D collision)
     {
@@ -25,33 +15,32 @@ public class ClimbCheck : MonoBehaviour
         {
             if (this.gameObject.GetComponentInParent<PlayerDataHolder>().ClimbWall == true && this.gameObject.GetComponentInParent<PlayerMovement>().Grounded == false)
             {
-                this.gameObject.GetComponentInParent<PlayerMovement>().DoubleJumpCount = 0;
-                this.gameObject.GetComponentInParent<Rigidbody2D>().velocity = new Vector2(0,0);
-                this.gameObject.GetComponentInParent<Rigidbody2D>().gravityScale = 0;
-                this.gameObject.GetComponentInParent<PlayerMovement>().Climbing = true;
-                this.gameObject.GetComponentInParent<PlayerMovement>().Grounded = false;
                 if (collision.gameObject.tag == "Ground")
                 {
                     this.gameObject.GetComponentInParent<PlayerMovement>().DoubleJumpCount = 0;
-                    this.gameObject.GetComponentInParent<Rigidbody2D>().velocity = new Vector2(0, 0);
+                    this.gameObject.GetComponentInParent<Rigidbody2D>().velocity = vector2;
                     this.gameObject.GetComponentInParent<Rigidbody2D>().gravityScale = 0;
                     this.gameObject.GetComponentInParent<PlayerMovement>().Climbing = true;
                     this.gameObject.GetComponentInParent<PlayerMovement>().Grounded = false;
                 }
             }
         }
+
+        if (Input.GetKeyUp(KeyCode.W) || (this.gameObject.GetComponentInParent<PlayerMovement>().Grounded == true))
+        {
+            this.gameObject.GetComponentInParent<PlayerMovement>().Climbing = false;
+
+        }
+
+
     }
 
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (this.gameObject.GetComponentInParent<PlayerDataHolder>().ClimbWall == true)
-        {
-            if (collision.gameObject.tag == "Ground")
-            {
-                this.gameObject.GetComponentInParent<Rigidbody2D>().gravityScale = 3;
-                this.gameObject.GetComponentInParent<PlayerMovement>().Climbing = false;
-            }
-        }
+
+        this.gameObject.GetComponentInParent<PlayerMovement>().Climbing = false;
+
     }
+
 }
